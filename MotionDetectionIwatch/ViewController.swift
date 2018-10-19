70d741d7e1e348ae28b7cb6ced3e747d69adc263
Ido Sakazi <idosakazi@idos-macbook-pro.local>
2018-10-19 14:10:20 +0300

    Initial Commit

---
+++ MotionDetectionIwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotionDetectionIwatch
+//
+//  Created by Ido Sakazi on 19/10/2018.
+//  Copyright © 2018 Ido Sakazi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
